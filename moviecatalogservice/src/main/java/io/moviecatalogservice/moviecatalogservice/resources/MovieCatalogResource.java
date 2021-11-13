aee959af0b0562c0490496737739dfbbc1f36048
muslum10cel <muslumoncel10@gmail.com>
2021-11-13 20:05:58 +0300

    Rest template added

--- moviecatalogservice/src/main/java/io/moviecatalogservice/moviecatalogservice/resources/MovieCatalogResource.java
+++ moviecatalogservice/src/main/java/io/moviecatalogservice/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,21 +1,19 @@
 package io.moviecatalogservice.moviecatalogservice.resources;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 import io.moviecatalogservice.Models.CatalogItem;
 import io.moviecatalogservice.Models.CatalogItems;
 import io.moviecatalogservice.Models.Movies;
 import io.moviecatalogservice.Models.UserRating;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/catalog")
@@ -24,33 +22,21 @@ public class MovieCatalogResource {
     private static final String RATING_URL = "http://localhost:1071/ratings/foo";
     private static final String MOVIES_URL = "http://localhost:1453/movies/foo";
     
-    private List<CatalogItem> catalogList =new ArrayList<>();
-
-    private MovieCatalogResource(){
-        catalogList.add(new CatalogItem("Transformers", "description", 4));
-    }
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/{userID}")
     public CatalogItems getCatalog(@PathVariable String userID){
         
-        
-        WebClient webClient = WebClient.builder().defaultHeader(HttpHeaders.USER_AGENT, "WebClient")
-        .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json").build();
-
         CatalogItems cItems = new CatalogItems();
         List<CatalogItem> cList = new ArrayList<>();
-        Flux<UserRating> uuu = webClient.get().uri(URI.create(RATING_URL)).retrieve().bodyToFlux(UserRating.class);
-        uuu.subscribe(u -> {
-            u.getRatings().stream().forEach(rate->{
-                Flux<Movies> movies = webClient.get().uri(URI.create(MOVIES_URL + rate.getMovieID())).retrieve().bodyToFlux(Movies.class);
-                movies.subscribe(movie -> {
-                    System.out.println(movie);
-                     movie.getMovies().stream().forEach(mov->
-                        cList.add(new CatalogItem(mov.getName(), "Test", rate.getRating())));    
-                });            
+        UserRating ratings =  restTemplate.getForObject(RATING_URL, UserRating.class);
+        ratings.getRatings().forEach(rate ->{
+            Movies movies = restTemplate.getForObject(MOVIES_URL, Movies.class);
+            movies.getMovies().forEach(movie -> {
+                cList.add(new CatalogItem(movie.getName(), "Desc", rate.getRating()));
             });
         });
-        System.out.println(cList.size());
         cItems.setCatalogItems(cList);
         return cItems;
     }
